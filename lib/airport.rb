51bb26e6159495b888ba37a7189c34945d4ec593
Constantin <kostiantynkalinin@gmail.com>
2016-01-25 00:14:09 +0000

    Second challenge

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,11 +1,19 @@
 require_relative 'plane'
 
- class Airport
+class Airport
+attr_accessor :landed_planes
+def initialize
+  @landed_planes = []
+end
 
 def landing plane
-landed_planes = []
 landed_planes << plane
-
+'Instructions to land'
   end
 
+  def taking_off plane
+   departed_flights = []
+   departed_flights << plane
+   landed_planes.delete plane
+ end
  end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,31 @@
 require 'airport'
 
  describe Airport do
+   subject(:airport) { described_class.new }
+   let(:plane) { double :plane }
 
  describe 'landing' do
 
     it { is_expected.to respond_to(:landing).with(1).argument }
 
    it 'confirms that plane has landed' do
-    plane = double (:plane)
-     expect(subject.landing(plane)).to include plane
-    end
-  end
+     allow(plane).to receive(:landing)
+      subject.landing(plane)
+      expect(subject.landed_planes).to include plane
+       end
+end
+    describe 'taking off' do
 
-  end
+    it 'gives instructions to takeoff' do
+      expect(airport).to respond_to(:taking_off).with(1).argument
+    end
+
+    it 'confirm that plane is no longer in the airport' do
+      allow(plane).to receive(:landing)
+      allow(plane).to receive(:taking_off)
+      expect(airport.landed_planes).not_to include plane
+    end
+
+      end
+
+        end
